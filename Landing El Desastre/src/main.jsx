e6cea30c0aa89b16eae71f33042eeaa67141b7f9
queridocava <hello.kvastudios@gmail.com>
2025-09-20 16:04:47 -0600

    Add files via upload

---
+++ Landing El Desastre/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ElDesastreLanding from './ElDesastreLanding'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ElDesastreLanding />
+  </React.StrictMode>
+)
